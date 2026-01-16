55d2ed6b35a6f2e5bef8bf2eaf5e544d3fc0dab0
ahmedAlkhwalde <ahmedkhkh224411@gmail.com>
2026-01-16 21:29:19 +0300

    edit word

--- project algorithm/src/Computer.java
+++ project algorithm/src/Computer.java
@@ -46,8 +46,8 @@
 //     حساب القيمة المتوقعة للوحة بناءً على احتمالات نتائج رمي العصي
     private double chancenode(Board b, int d, boolean isMax) {
         double total = 0;
-        double[] probs = {1/16, 4/16, 6/16, 4/16, 1/16};
-        int[] moves = {5, 1, 2, 3, 4};
+        double[] probs = {4/16, 6/16, 4/16, 1/16, 1/16};
+        int[] moves = {1, 2, 3, 4, 5};
         for (int i = 0; i < moves.length; i++) {
             double score = moveNode(b, d, isMax, moves[i]);
             total += probs[i] * score;
